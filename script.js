740a535d2dafc470eb8314ca0b1589737a31ee26
Peter Neamtu <neamtupeter@gmail.com>
2023-04-06 19:07:17 +0300

    imageSlider

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ async function getData() {
 
 getData().then((products) => {
   if (products) {
-    console.log('Lista cu produce: ', products);
+    console.log('Lista cu produse: ', products);
     products.forEach((product) => {
       function createElement(tag, text) {
         const tagElement = document.createElement(tag);
@@ -13,7 +13,6 @@ getData().then((products) => {
         return tagElement;
       }
 
-      console.log(product);
       const productsList = document.querySelector('.products-container');
       const newArticle = document.createElement('article');
       const newImageSection = document.createElement('section');
@@ -25,69 +24,7 @@ getData().then((products) => {
       newTextSection.classList.add('text-container');
       newPriceBoxSection.classList.add('price-container');
 
-      const newImage = document.createElement('img');
-      const prevButton = document.createElement('button');
-      const nextButton = document.createElement('button');
-      const dotContainer = document.createElement('div');
-
-      let imageIndex = 0;
-      newImage.src = product.images[imageIndex];
-      prevButton.innerText = '<';
-      nextButton.innerText = '>';
-
-      prevButton.classList.add('prev-btn');
-      nextButton.classList.add('next-btn');
-      dotContainer.classList.add('dot-container');
-
-      prevButton.addEventListener('click', previousImage);
-      nextButton.addEventListener('click', nextImage);
-      createDotsForImages();
-
-      let dotArr = Array.from(dotContainer.childNodes);
-      dotArr[0].classList.add('active');
-
-      function createDotsForImages() {
-        for (let i = 0; i < product.images.length; i++) {
-          const dot = document.createElement('span');
-          dot.classList.add('dot');
-          dotContainer.append(dot);
-          dot.addEventListener('click', () => {
-            newImage.src = product.images[i];
-            dotArr.forEach((elem) => elem.classList.remove('active'));
-            dotArr[i].classList.add('active');
-          });
-        }
-      }
-
-      function previousImage() {
-        if (imageIndex === 0) {
-          imageIndex = product.images.length - 1;
-          newImage.src = product.images[imageIndex];
-          dotArr[imageIndex].classList.add('active');
-          dotArr[0].classList.remove('active');
-        } else {
-          imageIndex -= 1;
-          newImage.src = product.images[imageIndex];
-          dotArr[imageIndex].classList.add('active');
-          dotArr[imageIndex + 1].classList.remove('active');
-        }
-      }
-
-      function nextImage() {
-        if (imageIndex === product.images.length - 1) {
-          imageIndex = 0;
-          newImage.src = product.images[imageIndex];
-          dotArr[imageIndex].classList.add('active');
-          dotArr[product.images.length - 1].classList.remove('active');
-        } else {
-          imageIndex += 1;
-          newImage.src = product.images[imageIndex];
-          dotArr[imageIndex].classList.add('active');
-          dotArr[imageIndex - 1].classList.remove('active');
-        }
-      }
-
-      newImageSection.append(newImage, prevButton, nextButton, dotContainer);
+      createImageSlider(product, newImageSection);
 
       newTextSection.append(
         createElement('h2', product.title),
@@ -97,56 +34,34 @@ getData().then((products) => {
       newPriceBoxSection.append(
         createElement('p', `Price: ${product.price}`),
         createElement('p', `Discount: ${product.discountPercentage}`),
-        createElement('p', `Discount price: ${calculateDiscountedPrice()}`),
+        createElement(
+          'p',
+          `Discount price: ${calculateDiscountedPrice(product)}`
+        ),
         createElement('p', `Stock: ${product.stock}`),
         createElement('p', `Rating: ${product.rating}`)
       );
 
-      // Punctul 7 din lista de task-uri
-
       const detailsArr = Array.from(newPriceBoxSection.childNodes);
 
-      // function calculateDiscountedPrice() {
-      //   if (!product.discountPercentage) {
-      //     detailsArr[2].style.display = 'none';
+      toggleDiscountPriceDisplay();
 
-      //     return;
-      //   } else {
-      //     detailsArr[0].classList.add('price');
-
-      //     return (
-      //       ((100 - product.discountPercentage) / 100) *
-      //       product.price
-      //     ).toFixed(2);
-      //   }
-      // }
-
-      function calculateDiscountedPrice() {
-        return (
-          ((100 - product.discountPercentage) / 100) *
-          product.price
-        ).toFixed(2);
+      function toggleDiscountPriceDisplay() {
+        if (!product.discountPercentage) {
+          detailsArr[2].style.display = 'none';
+          return;
+        }
+        detailsArr[0].classList.add('price');
       }
 
       newArticle.append(newImageSection, newTextSection, newPriceBoxSection);
 
       productsList.appendChild(newArticle);
 
-      function createModal(id) {
-        // Product
+      newTextSection.addEventListener('click', productDetails);
 
-        const product = products.filter((product) => product.id == id);
-
-        const modal = document.createElement('div');
-        const titleModal = document.createElement('h1');
-        const exitBtn = document.createElement('button');
-        const price = document.createElement('span');
-        const modalImage = document.createElement('div');
-
-        modal.classList.add('modalProduct');
-        modalImage.classList.add('modalImage');
-
-        modalImage.append(newImage);
+      function productDetails() {
+        location.href = `productDetails.html?productId=${product.id}`;
       }
     });
   }
