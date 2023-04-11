58b607f322324dc194cfbae0b83486ab36d0ecea
ioanacretu06 <ioanacretu06@gmail.com>
2023-04-11 20:04:34 +0300

    add-dropdown-style

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ getData().then((products) => {
     }
 
     const categoryList = document.createElement("ul");
-   
+    categoryList.classList.add("dropdownContent");
     const categories = [
       ...new Set(products.map((product) => product.category)),
     ];
@@ -172,16 +172,17 @@ getData().then((products) => {
 
     const categoryText = document.createElement("span");
     categoryText.textContent = "Category:";
+    categoryText.style.fontWeight = "bold";
     const filterDropdownMenuCat = document.querySelector(
       ".filter-dropdown-menu"
     );
 
-    filterDropdownMenuCat.appendChild(categoryText);
     filterDropdownMenuCat.appendChild(categoryList);
 
+    categoryList.insertBefore(categoryText, categoryList.firstChild);
     function createFilter(filterRanges, filterType, productsList, products) {
       const filterList = document.createElement("ul");
-
+      filterList.classList.add("dropdownContent");
       filterList.addEventListener("change", () => {
         const selectedCategories = Array.from(
           categoryList.querySelectorAll('input[type="checkbox"]:checked')
@@ -235,31 +236,32 @@ getData().then((products) => {
       filterText.textContent = `${
         filterType.charAt(0).toUpperCase() + filterType.slice(1)
       }:`;
+      filterText.style.fontWeight = "bold";
 
       const filterDropdownMenu = document.querySelector(
         ".filter-dropdown-menu"
       );
-      filterDropdownMenu.appendChild(filterText);
       filterDropdownMenu.appendChild(filterList);
+      filterList.insertBefore(filterText, filterList.firstChild);
     }
 
     const priceRanges = [
-      { label: "Under 100", range: [0, 100] },
+      { label: "under 100", range: [0, 100] },
       { label: "100 - 500", range: [100, 500] },
       { label: "500 - 1000", range: [500, 1000] },
-      { label: "Over 1000", range: [1000, Infinity] },
+      { label: "over 1000", range: [1000, Infinity] },
     ];
     const ratingRanges = [
-      { label: "1 star", range: [0, 1] },
-      { label: "2 stars", range: [1, 2] },
-      { label: "3 stars", range: [2, 3] },
-      { label: "4 stars", range: [3, 4] },
-      { label: "5 stars", range: [4, 5] },
+      { label: "\u2B50", range: [0, 1] },
+      { label: "\u2B50\u2B50", range: [1, 2] },
+      { label: "\u2B50\u2B50\u2B50", range: [2, 3] },
+      { label: "\u2B50\u2B50\u2B50\u2B50", range: [3, 4] },
+      { label: "\u2B50\u2B50\u2B50\u2B50\u2B50", range: [4, 5] },
     ];
     const stockRanges = [
-      { label: "Unavailable", range: [0, 0] },
-      { label: "Only a few left", range: [0, 50] },
-      { label: "Available", range: [50, Infinity] },
+      { label: "unavailable", range: [0, 0] },
+      { label: "only a few left", range: [0, 50] },
+      { label: "available", range: [50, Infinity] },
     ];
     createFilter(priceRanges, "price", productsList, products);
     createFilter(stockRanges, "stock", productsList, products);
