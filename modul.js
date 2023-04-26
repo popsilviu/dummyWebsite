6df776e9d360c64c6c97684821cd6b1fed5f6505
IsaiuAntonio <isaiu.antonio@gmail.com>
2023-04-26 16:40:11 +0300

    modal from task10 to main

--- modul.js
+++ modul.js
@@ -1,26 +1,7 @@
-function showProducts(products) {
+getData().then((products) => {
+  if (products) {
+    products.forEach((product) => {
 
-  products.forEach((product) => {
-    function createElement(tag, text) {
-      const tagElement = document.createElement(tag);
-      tagElement.innerText = text;
-      return tagElement;
-    }
-
-    const productsList = document.querySelector('.products-container');
-    const newArticle = document.createElement('article');
-    const newImageSection = document.createElement('section');
-    const newTextSection = document.createElement('section');
-    const newPriceBoxSection = document.createElement('section');
-
-    newArticle.setAttribute('id', product.id);
-    newImageSection.classList.add('image-container');
-    newTextSection.classList.add('text-container');
-    newPriceBoxSection.classList.add('price-container');
-
-
-    createImageSlider(product, newImageSection);
-    
     newTextSection.append(createElement('h2', product.title));
 
     const modalDetails = createElement("div");
@@ -157,34 +138,6 @@ function showProducts(products) {
     );
     modalImageContainer.append(modalPrev, modalImage, modalNext);
 
-  
-    newPriceBoxSection.append(
-      createElement('p', `Price: ${product.price}`),
-      createElement('p', `Discount: ${product.discountPercentage}`),
-      createElement('p', `Discount price: ${calculateDiscountedPrice(product)}`),
-      createElement('p', `Stock: ${product.stock}`),
-      createElement('p', `Rating: ${product.rating}`)
-    );
-
-    const detailsArr = Array.from(newPriceBoxSection.childNodes);
-
-    toggleDiscountPriceDisplay();
-
-    function toggleDiscountPriceDisplay() {
-      if (!product.discountPercentage) {
-        detailsArr[2].style.display = 'none';
-        return;
-      }
-      detailsArr[0].classList.add('price');
-    }
-
-    newArticle.append(newImageSection, newTextSection, newPriceBoxSection);
-    productsList.appendChild(newArticle);
-
-    // newTextSection.addEventListener('click', productDetails);
-
-    function productDetails() {
-      location.href = `productDetails.html?productId=${product.id}`;
-    }
   });
 }
+});
