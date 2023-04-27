1d7464d0178807683edafa38f2fb6af756af506c
alexandravanea <120100324+alexandravanea@users.noreply.github.com>
2023-04-27 16:59:21 +0300

    step 5

--- shop.js
+++ shop.js
@@ -1,10 +1,3 @@
-getData().then((products) => {
-    
-    if (products) {
-      
-      products.forEach((product) => {
-
-   
 const navbar = document.querySelector(".navbar");
 const cartIcon = document.createElement("div");
 cartIcon.setAttribute("id", "cart-nav-icon-container");
@@ -19,7 +12,6 @@ const cartWindow = document.createElement("div");
 cartWindow.setAttribute("class", "cart-window-container hide");
 cartIcon.appendChild(cartWindow);
 cartWindow.inWindow = 0;
-
 
 cartIcon.addEventListener("mouseover", () => {
   if (cartWindow.classList.contains("hide"))
@@ -62,120 +54,3 @@ const checkout = document.createElement("div");
 checkout.setAttribute("class", "checkout");
 checkout.innerText = "Checkout";
 cartWindow.appendChild(checkout);
-
-
-
-let productsInCart = [];
-const cartButton = document.createElement("button");
-    cartButton.classList.add("buy-btn");
-    cartButton.innerHTML = `<img id='cart-button' src='https://cdn-icons-png.flaticon.com/512/5465/5465858.png'>`;
-    cartButton.setAttribute("id", "cart-button-container");
-   
- newArticle.append(cartButton);
-   
-    
-cartButton.onclick = function () {
-    //createImageSlider(product, newImageSection) ;
-    let prodToCart = {
-      name: product.title,
-      id: product.id,
-      image: product.images[0],
-      count: 1,
-      price: calculateDiscountedPrice(product),
-      unitPrice: product.price,
-   }
-    updateProductsToArray(prodToCart);
-    updateProductsToShoppingCart();
-  };
-  
-  
-  checkout.onclick=function(){   //not working
-   let win= window.open('cart.html');
-   
-    //console.log(cartWrapper.innerHTML);
-          }
-
-  
- 
-  function updateProductsToArray(prodToCart) {
-    for (let i = 0; i < productsInCart.length; i++) {
-      if (productsInCart[i].id === product.id) {
-        productsInCart[i].count += 1;
-         return;
-       }
-     }
-     productsInCart.push(prodToCart);
-    // updateProductsToShoppingCart();
-   }
-  
-   let countTheSumPrice = function () { 
-    let sum = 0;
-    productsInCart.forEach((product) => {
-      sum += Number(product.price * product.count);
-    });
-    return sum.toFixed(2);
-  }
-
-  let updateProductsToShoppingCart = function () {  
-          if (productsInCart.length > 0) {
-      let result = productsInCart.map((product) => {
-            return `
-              <li class="cart-li-${product.id}">
-                <img class="cart-wrapper-img" src="${product.image}">
-                <h5 class="item-cart-title">${product.name}</h5>
-                <div class="box-price-quantity">
-                  <h6 class="item-cart-price">$${(product.price*product.count).toFixed(2)}</h6>
-                  <div class="quantity">
-                    <button class="button-minus-${product.id}"> - </button>
-                    <span class="countOfProduct">${product.count}</span>
-                    <button class="button-plus-${product.id}"> + </button>
-                  </div>
-                </div>
-              </li>`;
-          });
-
-      cartWrapper.innerHTML = result.join("");
-      cartWrapper.classList.remove('hide');
-      total.innerHTML = "TOTAL: $" + countTheSumPrice();
-    } else {
-          cartWrapper.classList.add('hide');
-          cartWrapper.innerHTML = '<h4 class="empty">Your shopping cart is empty</h4>';
-          total.innerHTML = "Total : $0.00 ";
-        }
-  };
-
-  cartWrapper.addEventListener("click", (e) => {
-    // Last
-    const isPlusButton = e.target.classList.contains(`button-plus-${product.id}`);
-    const isMinusButton = e.target.classList.contains(`button-minus-${product.id}`);
-        if (isPlusButton || isMinusButton) {
-          for (let i = 0; i < productsInCart.length; i++) {
-        if (productsInCart[i].id == product.id) {
-              if (isPlusButton) {
-                console.log("id produs: ", isPlusButton)
-                 productsInCart[i].count += 1;
-                
-                } else if (isMinusButton) {
-                  productsInCart[i].count -= 1;
-                  console.log("count = ", productsInCart[i].count);
-              }
-              //updateProductsToArray(productsInCart);
-              
-            }
-            if (productsInCart[i].count <= 0) {
-              productsInCart.splice(i, 1);
-              console.log(productsInCart.splice(i, 1))                                                               
-            }
-            
-          }
-          updateProductsToShoppingCart();
-        
-        }
-       });
-    
-  });
-    }
-});
- 
-
-
--- showProducts.js
+++ showProducts.js
@@ -1,3 +1,4 @@
+let productsInCart = [];
 function showProducts(products) {
   products.forEach((product) => {
     function createElement(tag, text) {
@@ -13,7 +14,7 @@ function showProducts(products) {
     const newPriceBoxSection = document.createElement("section");
 
     newArticle.setAttribute("id", product.id);
-    
+
     newImageSection.classList.add("image-container");
     newTextSection.classList.add("text-container");
     newPriceBoxSection.classList.add("price-container");
@@ -51,12 +52,110 @@ function showProducts(products) {
     newArticle.append(newImageSection, newTextSection, newPriceBoxSection);
     productsList.appendChild(newArticle);
 
+    const cartButton = document.createElement("button");
+    cartButton.classList.add("buy-btn");
+    cartButton.innerHTML = `<img id='cart-button' src='https://cdn-icons-png.flaticon.com/512/5465/5465858.png'>`;
+    cartButton.setAttribute("id", "cart-button-container");
+    newArticle.append(cartButton);
+
     newTextSection.addEventListener("click", productDetails);
 
     function productDetails() {
       location.href = `productDetails.html?productId=${product.id}`;
     }
-    //buy-button
-    
+
+    cartButton.onclick = function () {
+      let prodToCart = {
+        name: product.title,
+        id: product.id,
+        image: product.images[0],
+        count: 1,
+        price: calculateDiscountedPrice(product),
+        unitPrice: product.price,
+      };
+      updateProductsToArray(prodToCart);
+      updateProductsToShoppingCart();
+    };
+
+    function updateProductsToArray(prodToCart) {
+      for (let i = 0; i < productsInCart.length; i++) {
+        if (productsInCart[i].id === product.id) {
+          productsInCart[i].count += 1;
+          return;
+        }
+      }
+      productsInCart.push(prodToCart);
+      // updateProductsToShoppingCart();
+    }
+
+    let countTheSumPrice = function () {
+      let sum = 0;
+      productsInCart.forEach((product) => {
+        sum += Number(product.price * product.count);
+      });
+      return sum.toFixed(2);
+    };
+
+    let updateProductsToShoppingCart = function () {
+      if (productsInCart.length > 0) {
+        let result = productsInCart.map((product) => {
+          return `
+                    <li class="cart-li-${product.id}">
+                      <img class="cart-wrapper-img" src="${product.image}">
+                      <h5 class="item-cart-title">${product.name}</h5>
+                      <div class="box-price-quantity">
+                        <h6 class="item-cart-price">$${(
+                          product.price * product.count
+                        ).toFixed(2)}</h6>
+                        <div class="quantity">
+                          <button class="button-minus-${
+                            product.id
+                          }"> - </button>
+                          <span class="countOfProduct">${product.count}</span>
+                          <button class="button-plus-${product.id}"> + </button>
+                        </div>
+                      </div>
+                    </li>`;
+        });
+
+        cartWrapper.innerHTML = result.join("");
+        cartWrapper.classList.remove("hide");
+        total.innerHTML = "TOTAL: $" + countTheSumPrice();
+      } else {
+        cartWrapper.classList.add("hide");
+        cartWrapper.innerHTML =
+          '<h4 class="empty">Your shopping cart is empty</h4>';
+        total.innerHTML = "Total : $0.00 ";
+      }
+    };
+
+    cartWrapper.addEventListener("click", (e) => {
+      // Last
+      const isPlusButton = e.target.classList.contains(
+        `button-plus-${product.id}`,
+      );
+      const isMinusButton = e.target.classList.contains(
+        `button-minus-${product.id}`,
+      );
+      if (isPlusButton || isMinusButton) {
+        for (let i = 0; i < productsInCart.length; i++) {
+          if (productsInCart[i].id == product.id) {
+            if (isPlusButton) {
+              console.log("id produs: ", isPlusButton);
+              productsInCart[i].count += 1;
+            } else if (isMinusButton) {
+              productsInCart[i].count -= 1;
+              console.log("count = ", productsInCart[i].count);
+            }
+            //updateProductsToArray(productsInCart);
+          }
+          if (productsInCart[i].count <= 0) {
+            productsInCart.splice(i, 1);
+            console.log(productsInCart.splice(i, 1));
+          }
+        }
+        updateProductsToShoppingCart();
+      }
+    });
   });
 }
